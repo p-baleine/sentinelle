70be1b77c378144038c5d4b1a6d3330303399e68
p-baleine <p.baleine@gmail.com>
2020-03-01 02:25:00 +0900

    古いバージョンの grpc で wait_for_termination が server にない場合に対応

--- sentinelle/servicer.py
+++ sentinelle/servicer.py
@@ -1,6 +1,7 @@
 from concurrent import futures
 import logging
 import sys
+import time
 
 import grpc
 
@@ -11,15 +12,11 @@ from sentinelle.inspectors import InspectorProto
 from sentinelle import interceptors
 
 # TODO: logger!!!
-# TODO: github にある gRPC 公式の examples を一通り目を通す
-# TODO: scouty で試す
 
 # NOTE: 実行履歴を持ちたい
 # 実行履歴が鍵になる気がしている。
 # + 今回実行すべきテスト対象 :: 前回から今回で差分のあったファイルの影響範囲
 # + テスト落ちたときに知りたいこと :: green から red になったタイミングのファイルの変更範囲
-# などなど。「実行履歴」じゃなくて、sentinelle っぽい名前が良いなぁ。
-# 全体的に servicer からロジックを切り出して、sentinelle っぽくしたい(楽しそう)
 
 logger = logging.getLogger(__name__)
 
@@ -57,12 +54,9 @@ class Servicer(sentinelle_pb2_grpc.SentinelleServicer):
             if m in sys.modules:
                 logger.debug(f'Invalidate {m}')
                 utils.deep_reload(sys.modules[m])
-                # importlib.reload(sys.modules[m])
 
 
 # FIXME: 限りなくシングルスレッドを期待している
-# TODO: そもそも gRPC python ってデフォルトでは並列処理まわりどうなっているか調べる
-# ThreadPoolExecutor かぁ
 def serve(avec: InspectorProto):
     server = grpc.server(
         futures.ThreadPoolExecutor(max_workers=10),
@@ -71,7 +65,17 @@ def serve(avec: InspectorProto):
         Servicer(avec), server)
     server.add_insecure_port('[::]:50051')
     server.start()
-    # TODO: wait_for_termination は experimental で古いバージョンだと存在しないため
-    # バージョン毎に切り分けする
-    # https://github.com/grpc/grpc/blob/8aff9ec59220d3394d36c1e43c1a5fbb3c2e9574/src/python/grpcio/grpc/__init__.py#L1456
-    server.wait_for_termination()
+
+    try:
+        server.wait_for_termination()
+    except AttributeError as e:
+        if str(e) == ('\'_Server\' object has no attribute'
+                      ' \'wait_for_termination\''):
+            # https://github.com/grpc/grpc/blob/5b9b5ebe26b3c84477d989fbcda5959e9ec00adf/examples/python/data_transmission/server.py#L105
+            try:
+                while 1:
+                    time.sleep(10)
+            except KeyboardInterrupt:
+                server.stop(0)
+        else:
+            raise e
